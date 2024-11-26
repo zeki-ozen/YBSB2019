f4eeced3ef3f47926f3c997b05dbe761c1cf75cd
Zeki Özen <1252016+zeki-ozen@users.noreply.github.com>
2024-11-27 00:07:08 +0300

    Add files via upload

---
+++ W8/Method_by_Returning_Value/Program.cs
@@ -0,0 +1,35 @@
+﻿/********** STRUCTURED PROGRAMMING **********************/
+/********** Dr. Zeki Ozen *******************************/
+/********** WEEK 8 - Methods by return value types ******/
+
+namespace Method_with_Returning_Value
+{
+    internal class Program
+    {
+
+        public static int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+
+        public static void PrintResult(int result)
+        {
+            Console.WriteLine($"Result: {result}");
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the first number: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int result = Sum(number1, number2);
+
+            PrintResult(result);
+
+        }
+    }
+}
