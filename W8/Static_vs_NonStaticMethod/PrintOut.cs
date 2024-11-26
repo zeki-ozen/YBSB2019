07fbe17de526c96b2f99b8b56ec24f6490fd7e35
Zeki Özen <1252016+zeki-ozen@users.noreply.github.com>
2024-11-27 00:07:42 +0300

    Add files via upload

---
+++ W8/Static_vs_NonStaticMethod/PrintOut.cs
@@ -0,0 +1,18 @@
+﻿namespace staticMethod
+{
+    internal class PrintOut
+    {
+
+        // Static Method
+        public static void PrintInfo()
+        {
+            Console.WriteLine("Static Method");
+        }
+
+        // Instance Method
+        public void Display()
+        {
+            Console.WriteLine("Instance Method");
+        }
+    }
+}
---
+++ W8/Static_vs_NonStaticMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿/********** STRUCTURED PROGRAMMING ****************/
+/********** Dr. Zeki Ozen *************************/
+/********** WEEK 8 - Static vs NonStatic Methods **/
+
+namespace staticMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the static method
+            PrintOut.PrintInfo();
+
+            // Create an object of the PrintOut class
+            PrintOut prntout = new PrintOut();
+
+            // Call the instance method
+            prntout.Display();
+        }
+    }
+}
