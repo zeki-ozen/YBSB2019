2fe6e6db92a8eb4ab4a87e244638a5079fa11c25
Zeki Özen <1252016+zeki-ozen@users.noreply.github.com>
2024-11-27 16:11:33 +0300

    Add files via upload

---
+++ W8/CalculateSphereArea/Program.cs
@@ -0,0 +1,25 @@
+﻿
+internal class Program
+{
+
+    public static double CalculateSphereArea(double r)
+    {
+        return (4.0 / 3.0) * Math.PI * r * r;
+    }
+
+    public static void PrintOutArea(double area)
+    {
+        Console.WriteLine($"The area of the Sphere is {area}");
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Type r of the Sphere");
+        double r = double.Parse(Console.ReadLine());
+
+        double area = CalculateSphereArea(r);
+        //Console.WriteLine($"Area of the sphere is {area}");
+        PrintOutArea(area);
+
+    }
+}
